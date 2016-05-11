6151fcc0ac62aaf2ab6ca402341087ef76b7fe6b
RadSt <desan1986@gmail.com>
2016-05-11 11:34:30 +0300

    Start working on site

---
+++ MVC ASP.NET Practise/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_ASP.NET_Practise.MvcApplication" Language="C#" %>
---
+++ MVC ASP.NET Practise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_ASP.NET_Practise.Startup))]
+namespace MVC_ASP.NET_Practise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
