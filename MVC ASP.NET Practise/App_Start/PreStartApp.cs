4ee7380f4bb6323228fae09ae5e54f04310c7d70
RadSt <desan1986@gmail.com>
2016-05-11 12:35:14 +0300

    End less 1 from ChernikovMVC

---
+++ MVC ASP.NET Practise/App_Start/PreStartApp.cs
@@ -0,0 +1,19 @@
+﻿using MVC_ASP.NET_Practise;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(PreStartApp), "Start")]
+namespace MVC_ASP.NET_Practise
+{
+    public static class PreStartApp
+    {
+        static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        //// <summary>
+        //// Метод запукаеться один раз перед стартом приложения
+        //// </summary>
+        public static void Start()
+        {
+            logger.Info("Application Prestart");
+        }
+
+    }
+}
--- MVC ASP.NET Practise/Global.asax.cs
+++ MVC ASP.NET Practise/Global.asax.cs
@@ -1,21 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NLog;
 
 namespace MVC_ASP.NET_Practise
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
+        Logger logger = LogManager.GetCurrentClassLogger();
+
         protected void Application_Start()
         {
+            logger.Info("Application Start");
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public void Init()
+        {
+            logger.Info("application Init");
+        }
+
+        public void Dispose()
+        {
+            logger.Info("Application Dispose");
+        }
+
+        public void Application_Errror()
+        {
+            logger.Info("Application Error");
+        }
+
+        public void Application_End()
+        {
+            logger.Info("Application End");
         }
     }
 }
