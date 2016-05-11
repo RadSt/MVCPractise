3b17ab2e31772081391504d34894da9281b4d402
Radik <desan1986@gmail.com>
2016-05-12 02:29:39 +0300

    Start les3

---
+++ Lesson3/WebApp/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IWeapon Weapon { get; set; }
+        public HomeController()
+        {
+            Weapon = DependencyResolver.Current.GetService<IWeapon>();
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(Weapon);
+        }
+    }
+}
