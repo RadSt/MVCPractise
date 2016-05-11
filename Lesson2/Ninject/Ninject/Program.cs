7069bd883dd151587251d4c0cd98479723e196a3
RadSt <desan1986@gmail.com>
2016-05-11 14:05:40 +0300

    Start Unity Inj App

---
+++ Lesson2/Ninject/Ninject/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+
+namespace Ninject
+{
+    interface IWeapon
+    {
+        void Kill();
+    }
+
+    class Bazuka : IWeapon
+    {
+        public void Kill()
+        {
+            Console.WriteLine("BIG BADABUM!");
+        }
+    }
+
+    class Sword : IWeapon
+    {
+        public void Kill()
+        {
+            Console.WriteLine("Chunk-chunk");
+        } 
+    }
+
+    class Warior
+    {
+        readonly IWeapon weapon;
+
+        public Warior(IWeapon weapon)
+        {
+            this.weapon = weapon;
+        }
+
+        public void Kill()
+        {
+              weapon.Kill();
+        }
+    }
+
+    class OtherWarrior
+    {
+        /// <summary>
+        /// Метод с получением интерфейса при необходимости обращается 
+        /// к сервис локатору и получает его
+        /// </summary>
+        private IWeapon _weapon;
+
+        public IWeapon Weapon
+        {
+            get
+            {
+                if (_weapon == null)
+                {
+                    _weapon = Program.AppKernel.Get<IWeapon>();
+                }
+                return _weapon;
+            }
+        }
+
+        public void Kill()
+        {
+            Weapon.Kill();
+        }
+    }
+
+    class AnotherWarrior
+    {
+        /// <summary>
+        /// То же самое с помощью [Inject] при создании класса 
+        /// через AppKernel.Get<>
+        /// поле инициализируется сервис локатором.
+        /// </summary>
+        [Inject]
+        public IWeapon Weapon { get; set; }
+
+        public void Kill()
+        {
+            Weapon.Kill();
+        } 
+    }
+
+    class WeaponNinjectModule : NinjectModule
+    {
+        /// <summary>
+        /// Привязка интерфейса к классу с помощью Ninject 
+        /// </summary>
+        public override void Load()
+        {
+            this.Bind<IWeapon>().To<Sword>();
+        }
+    }
+    class Program
+    {
+        public static IKernel AppKernel; 
+        static void Main(string[] args)
+        {
+            AppKernel= new StandardKernel(new WeaponNinjectModule());
+            Warior warior = AppKernel.Get<Warior>();
+            warior.Kill();
+
+            OtherWarrior otherWarrior = new OtherWarrior();
+            otherWarrior.Kill();
+
+            AnotherWarrior anotherWarrior = AppKernel.Get<AnotherWarrior>();
+            anotherWarrior.Kill();
+
+            Console.ReadLine();
+        }
+    }
+}
--- Lesson2/Unity/Unity/Properties/AssemblyInfo.cs
+++ Lesson2/Unity/Unity/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lesson2")]
+[assembly: AssemblyTitle("Unity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lesson2")]
+[assembly: AssemblyProduct("Unity")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ed4fe5f5-88f8-4a21-8c95-14cb0a0036d6")]
+[assembly: Guid("e3957e24-110a-4119-9859-5d24a3754de5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
