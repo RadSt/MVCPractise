2d0348791d5e71ce917cb3fe71a7c672f60f79ff
Radik <desan1986@gmail.com>
2016-05-14 18:57:27 +0300

    End less5

--- Lesson5/WebApp/WebApp/Areas/Default/Controllers/UserController.cs
+++ Lesson5/WebApp/WebApp/Areas/Default/Controllers/UserController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Web.ModelBinding;
 using System.Web.Mvc;
 using WebApp.Controllers;
 using WebApp.Models.Abstract;
 using WebApp.Models.Enteties;
+using WebApp.Tools;
 using static System.String;
 
 namespace WebApp.Areas.Default.Controllers
@@ -35,7 +37,7 @@ namespace WebApp.Areas.Default.Controllers
         [HttpPost]
         public ActionResult Register(User user, string birthdayDay, string birthdayMonth, string birthdayYear)
         {
-            if (user.Captcha != "1234" )
+            if (user.Captcha != (string)Session[CaptchaImage.CaptchaValueKey] )
             {
                 ModelState.AddModelError("Captcha","Текст с картинки введен неверно!");
             }
@@ -54,6 +56,23 @@ namespace WebApp.Areas.Default.Controllers
             }
 
             return View(user);
+        }
+
+        public ActionResult Captcha()
+        {
+            Session[CaptchaImage.CaptchaValueKey] = new Random(DateTime.Now.Millisecond).Next(1111, 9999).ToString();
+            var ci = new CaptchaImage(Session[CaptchaImage.CaptchaValueKey].ToString(), 221, 50, "Arial");
+
+            // Change the response headers to output a JPEG image.
+            Response.Clear();
+            Response.ContentType = "image/jpg";
+
+            // Write the image to the response stream in JPEG format.
+            ci.Image.Save(Response.OutputStream, ImageFormat.Jpeg);
+
+            // Dispose of the CAPTCHA image object.
+            ci.Dispose();
+            return null;
         }
     }
 }
