b7f531c69e1cf422a9165531582a530d3d9786d7
Radik <desan1986@gmail.com>
2016-05-18 18:44:09 +0300

    Add Interface for CustomRoleProvider

---
+++ Lesson6/WebApp/WebApp/Models/Abstract/ICustomProfileProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace WebApp.Models.Abstract
+{
+    public interface ICustomProfileProvider
+    {
+        SettingsPropertyValueCollection GetPropertyValues(SettingsContext context,
+            SettingsPropertyCollection collection);
+
+        void SetPropertyValues(SettingsContext context, SettingsPropertyValueCollection collection);
+    }
+}
